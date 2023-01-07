40b9420c459e5548f3538a2e0ca700526d8b00e0
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-01-07 15:25:17 -0300

    feat: add UniqueEntityId

---
+++ src/domain/@shared/domain/index.ts
@@ -0,0 +1,2 @@
+export { default as UniqueEntityId } from './value-objects/unique-entity-id'
+export { default as ValueObject } from './value-objects/value-object'
---
+++ src/domain/@shared/domain/value-objects/unique-entity-id.ts
@@ -0,0 +1,22 @@
+import ValueObject from './value-object'
+import { InvalidUuidError } from '@/domain/@shared/errors'
+
+import { v4 as uuid, validate as uuidValidate } from 'uuid'
+
+export default class UniqueEntityId extends ValueObject<string> {
+  constructor (id?: string) {
+    super(id ?? uuid())
+    this.validate()
+  }
+
+  get id (): string {
+    return this.value
+  }
+
+  private validate (): void {
+    const isValid = uuidValidate(this.value)
+    if (!isValid) {
+      throw new InvalidUuidError()
+    }
+  }
+}
