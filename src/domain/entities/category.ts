6e24e73bfbe374ca39b256fc115be87c1ab1899e
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-01-07 16:46:52 -0300

    feat: add class Category

---
+++ src/domain/entities/category.ts
@@ -0,0 +1,39 @@
+import { Entity, UniqueEntityId } from '@/domain/@shared/domain'
+
+type CategoryProps = {
+  name: string
+  description?: string
+  is_active?: boolean
+  created_at?: Date
+}
+
+export class Category extends Entity<CategoryProps> {
+  private readonly _name: string
+  private readonly _description: string
+  private readonly _is_active: boolean
+  private readonly _created_at: Date
+
+  constructor (props: CategoryProps, id?: UniqueEntityId) {
+    super(props, id)
+    this._name = props.name
+    this._description = props.description ?? ''
+    this._is_active = props.is_active ?? false
+    this._created_at = props.created_at ?? new Date()
+  }
+
+  get name (): string {
+    return this._name
+  }
+
+  get description (): string {
+    return this._description
+  }
+
+  get is_active (): boolean {
+    return this._is_active
+  }
+
+  get created_at (): Date {
+    return this._created_at
+  }
+}
