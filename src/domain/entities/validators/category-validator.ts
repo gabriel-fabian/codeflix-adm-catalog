5f92af784a590e8ef742cecfc568c6864b4e4ea8
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-01-08 16:41:00 -0300

    feat: ensure CategoryValidator validates data

---
+++ src/domain/entities/validators/category-validator.ts
@@ -0,0 +1,36 @@
+import { ClassValidator } from '@/domain/@shared/validators'
+import { CategoryProps } from '@/domain/entities'
+import { IsBoolean, IsDate, IsNotEmpty, IsOptional, IsString, MaxLength } from 'class-validator'
+
+class ValidateProps {
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
+    name: string
+
+  @IsString()
+  @IsOptional()
+    description: string
+
+  @IsBoolean()
+  @IsOptional()
+    is_active: boolean
+
+  @IsDate()
+  @IsOptional()
+    created_at: Date
+
+  constructor ({ name, description, is_active, created_at }: CategoryProps) {
+    Object.assign(this, { name, description, is_active, created_at })
+  }
+}
+
+export class CategoryValidator extends ClassValidator<ValidateProps> {
+  validate (data: CategoryProps): boolean {
+    return super.validate(new ValidateProps(data))
+  }
+}
+
+export const makeCategoryValidator = (): CategoryValidator => {
+  return new CategoryValidator()
+}
