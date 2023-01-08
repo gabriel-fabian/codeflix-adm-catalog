da97aeb6d5054b16e22eee9e62b035fa5d662c25
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-01-08 13:54:18 -0300

    feat: ensure Category calls ValidatorRules

--- src/domain/entities/category.ts
+++ src/domain/entities/category.ts
@@ -1,4 +1,5 @@
 import { Entity, UniqueEntityId } from '@/domain/@shared/domain'
+import { ValidatorRules } from '../@shared/validators/validator-rules'
 
 type CategoryProps = {
   name: string
@@ -14,11 +15,31 @@ export class Category extends Entity<CategoryProps> {
   private readonly _created_at: Date
 
   constructor (props: CategoryProps, id?: UniqueEntityId) {
+    Category.validate(props)
     super(props, id)
     this._name = props.name
     this._description = props.description ?? ''
     this._is_active = props.is_active ?? false
     this._created_at = props.created_at ?? new Date()
+  }
+
+  update (name: string, description: string): void {
+    Category.validate({ name, description })
+    this._name = name
+    this._description = description
+  }
+
+  static validate (props: Omit<CategoryProps, 'created_at'>): void {
+    ValidatorRules.values(props.name, 'name').required().string().maxLength(255)
+    ValidatorRules.values(props.description, 'description').string()
+  }
+
+  activate (): void {
+    this._is_active = true
+  }
+
+  deactivate (): void {
+    this._is_active = false
   }
 
   get name (): string {
@@ -35,18 +56,5 @@ export class Category extends Entity<CategoryProps> {
 
   get created_at (): Date {
     return this._created_at
-  }
-
-  update (name: string, description: string): void {
-    this._name = name
-    this._description = description
-  }
-
-  activate (): void {
-    this._is_active = true
-  }
-
-  deactivate (): void {
-    this._is_active = false
   }
 }
