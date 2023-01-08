6c27d2451d2164ae4dbd26d992d25c5c28438dd9
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-01-08 11:44:21 -0300

    test: add ValidatorRules

---
+++ tests/@shared/validators/validator-rules.spec.ts
@@ -0,0 +1,138 @@
+import { ValidationError } from '@/domain/@shared/errors'
+import { ValidatorRules } from '@/domain/@shared/validators/validator-rules'
+
+const makeSut = (value: any, property: string): ValidatorRules => {
+  return ValidatorRules.values(value, property)
+}
+
+describe('ValidatorRules', () => {
+  test('values method', () => {
+    const value = ''
+    const property = 'field'
+
+    const validator = makeSut(value, property)
+
+    expect(validator).toBeInstanceOf(ValidatorRules)
+    expect(validator.value).toBe(value)
+    expect(validator.property).toBe(property)
+  })
+
+  describe('Required method', () => {
+    it('should throw an error when field is missing', () => {
+      const arrangeValues: any[] = [
+        '',
+        null,
+        undefined
+      ]
+      const property = 'field'
+
+      arrangeValues.forEach(value => {
+        const validator = makeSut(value, property)
+        expect(() => {
+          validator.required()
+        }).toThrowError(new ValidationError(`The ${property} is required`))
+      })
+    })
+
+    it('should not throw when param is valid', () => {
+      const value = 'valid param'
+      const property = 'field'
+
+      const validator = makeSut(value, property)
+
+      expect(() => {
+        validator.required()
+      }).not.toThrowError(new ValidationError(`The ${property} is required`))
+    })
+  })
+
+  describe('String method', () => {
+    it('should throw an error when param is not string', () => {
+      const value = 10
+      const property = 'field'
+
+      const validator = makeSut(value, property)
+
+      expect(() => {
+        validator.string()
+      }).toThrowError(new ValidationError(`The ${property} must be a string`))
+    })
+
+    it('should not throw an error when param is a string', () => {
+      const value = 'valid_value'
+      const property = 'field'
+
+      const validator = makeSut(value, property)
+
+      expect(() => {
+        validator.string()
+      }).not.toThrowError(new ValidationError(`The ${property} must be a string`))
+    })
+  })
+
+  describe('MaxLength method', () => {
+    it('should throw an error when param is greater than maxLenght', () => {
+      const value = 'thisIsGreaterThanMaxLength'
+      const property = 'field'
+      const maxLenght = 5
+
+      const validator = makeSut(value, property)
+
+      expect(() => {
+        validator.maxLength(maxLenght)
+      }).toThrowError(new ValidationError(`The ${property} must be less or equal than ${maxLenght} characters`))
+    })
+
+    it('should throw an error when param is greater than maxLenght', () => {
+      const value = 'thisIsLowerThanMaxLength'
+      const property = 'field'
+      const maxLenght = 255
+
+      const validator = makeSut(value, property)
+
+      expect(() => {
+        validator.maxLength(maxLenght)
+      }).not.toThrowError(new ValidationError(`The ${property} must be less or equal than ${maxLenght} characters`))
+    })
+  })
+
+  it('should should throw an error with combined rules and invalid values', () => {
+    const property = 'field'
+    const maxLenght = 5
+    const dataValues: Array<{ value: any, error: ValidationError }> = [
+      { value: '', error: new ValidationError(`The ${property} is required`) },
+      { value: true, error: new ValidationError(`The ${property} must be a string`) },
+      {
+        value: 'invalid_size',
+        error: new ValidationError(`The ${property} must be less or equal than ${maxLenght} characters`)
+      }
+    ]
+
+    dataValues.forEach(data => {
+      const validator = makeSut(data.value, property)
+      expect(() => {
+        validator.required().string().maxLength(maxLenght)
+      }).toThrowError(data.error)
+    })
+  })
+
+  it('should not throw an error with combined rules and valid values', () => {
+    const property = 'field'
+    const maxLenght = 50
+    const dataValues: Array<{ value: any, error: ValidationError }> = [
+      { value: 'any_value', error: new ValidationError(`The ${property} is required`) },
+      { value: 'any_string', error: new ValidationError(`The ${property} must be a string`) },
+      {
+        value: 'valid_size',
+        error: new ValidationError(`The ${property} must be less or equal than ${maxLenght} characters`)
+      }
+    ]
+
+    dataValues.forEach(data => {
+      const validator = makeSut(data.value, property)
+      expect(() => {
+        validator.required().string().maxLength(maxLenght)
+      }).not.toThrowError(data.error)
+    })
+  })
+})
