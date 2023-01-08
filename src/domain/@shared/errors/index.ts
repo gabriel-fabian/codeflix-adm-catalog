ef51b7e92aebf959e3af4b4ab97811763e07f97b
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-01-08 11:41:09 -0300

    feat: add ValidatorRules
    - Added a ValidationError class\n- Added a ValidatorRules class to validate required, string and the maxLenght of a given value

--- src/domain/@shared/errors/index.ts
+++ src/domain/@shared/errors/index.ts
@@ -1,1 +1,2 @@
 export { default as InvalidUuidError } from './invalid-uuid.error'
+export * from './validation-error'
---
+++ src/domain/@shared/validators/validator-rules.ts
@@ -0,0 +1,30 @@
+import { ValidationError } from '@/domain/@shared/errors'
+
+export class ValidatorRules {
+  private constructor (public readonly value: any, public readonly property: string) {}
+
+  static values (value: any, property: string): ValidatorRules {
+    return new ValidatorRules(value, property)
+  }
+
+  required (): this {
+    if (this.value === null || this.value === undefined || this.value === '') {
+      throw new ValidationError(`The ${this.property} is required`)
+    }
+    return this
+  }
+
+  string (): this {
+    if (typeof this.value !== 'string') {
+      throw new ValidationError(`The ${this.property} must be a string`)
+    }
+    return this
+  }
+
+  maxLength (max: number): this {
+    if (this.value.length > max) {
+      throw new ValidationError(`The ${this.property} must be less or equal than ${max} characters`)
+    }
+    return this
+  }
+}
