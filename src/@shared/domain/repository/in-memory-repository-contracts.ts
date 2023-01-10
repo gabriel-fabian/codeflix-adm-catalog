2a2499f223e4db9f64ad23cec33277396389d00d
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-01-10 17:13:49 -0300

    feat: add SearchResultParams

--- src/@shared/domain/repository/in-memory-repository-contracts.ts
+++ src/@shared/domain/repository/in-memory-repository-contracts.ts
@@ -94,7 +94,56 @@ export class SearchParams {
   }
 }
 
-export interface SearchableRepositoryInterface<E extends Entity, SearchInput, SearchOutput>
-  extends InMemoryRepositoryInterface<E> {
+type SearchResultProps<E extends Entity, Filter> = {
+  items: E[]
+  total: number
+  current_page: number
+  per_page: number
+  sort: string | null
+  sort_dir: string | null
+  filter: Filter
+}
+
+export class SearchResult<E extends Entity, Filter = string> {
+  readonly items: E[]
+  readonly total: number
+  readonly current_page: number
+  readonly per_page: number
+  readonly last_page: number
+  readonly sort: string | null
+  readonly sort_dir: string | null
+  readonly filter: Filter
+
+  constructor (props: SearchResultProps<E, Filter>) {
+    this.items = props.items
+    this.total = props.total
+    this.current_page = props.current_page
+    this.per_page = props.per_page
+    this.last_page = Math.ceil(this.total / this.per_page)
+    this.sort = props.sort
+    this.sort_dir = props.sort_dir
+    this.filter = props.filter
+  }
+
+  toJSON (forceEntity = false): object {
+    return {
+      items: forceEntity ? this.items.map((item) => item.toJSON()) : this.items,
+      total: this.total,
+      current_page: this.current_page,
+      per_page: this.per_page,
+      last_page: this.last_page,
+      sort: this.sort,
+      sort_dir: this.sort_dir,
+      filter: this.filter
+    }
+  }
+}
+
+export interface SearchableRepositoryInterface<
+  E extends Entity,
+  Filter = string,
+  SearchInput = SearchParams,
+  SearchOutput = SearchResult<E, Filter>
+> extends InMemoryRepositoryInterface<E> {
   search: (props: SearchInput) => Promise<SearchOutput>
 }
