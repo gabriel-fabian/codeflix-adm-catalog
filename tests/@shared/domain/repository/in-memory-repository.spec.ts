638bb3e82c71cf2eb27c53abf235e3482caa7368
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-01-10 13:17:08 -0300

    refactor: move SUT creation to a factory helper method

--- tests/@shared/domain/repository/in-memory-repository.spec.ts
+++ tests/@shared/domain/repository/in-memory-repository.spec.ts
@@ -9,16 +9,20 @@ class StubEntity extends Entity<StubEntityProps> {}
 
 class StubInMemoryRepository extends InMemoryRepository<StubEntity> {}
 
+const makeSut = (): InMemoryRepository<StubEntity> => {
+  return new StubInMemoryRepository()
+}
+
 describe('InMemoryRepository', () => {
   it('should insert a new entity', async () => {
-    const repository = new StubInMemoryRepository()
+    const sut = makeSut()
     const entity = new StubEntity({
       name: 'any_name',
       price: 10
     })
 
-    await repository.insert(entity)
+    await sut.insert(entity)
 
-    expect(entity.toJSON()).toStrictEqual(repository.items[0].toJSON())
+    expect(entity.toJSON()).toStrictEqual(sut.items[0].toJSON())
   })
 })
