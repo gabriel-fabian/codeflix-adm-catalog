b7f96f4329dedac46dcd24f3b2480242156d2538
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-01-11 12:57:21 -0300

    test: ensure CreateCategoryUseCase creates a category

---
+++ tests/application/usecases/create-category-usecase.spec.ts
@@ -0,0 +1,47 @@
+import { CreateCategoryUseCase } from '@/application/usecases'
+import { CategoryInMemoryRepository } from '@/infra/repos/in-memory'
+
+type SutTypes = {
+  sut: CreateCategoryUseCase
+  repository: CategoryInMemoryRepository
+}
+
+const makeSut = (): SutTypes => {
+  const repository = new CategoryInMemoryRepository()
+  const sut = new CreateCategoryUseCase(repository)
+
+  return {
+    sut,
+    repository
+  }
+}
+
+describe('CreateCategoryUseCase', () => {
+  it('should create a category', async () => {
+    const { sut, repository } = makeSut()
+
+    let output = await sut.run({ name: 'any_category' })
+
+    expect(output).toStrictEqual({
+      id: repository.items[0].id,
+      name: 'any_category',
+      description: '',
+      is_active: false,
+      created_at: repository.items[0].created_at
+    })
+
+    output = await sut.run({
+      name: 'any_category',
+      description: 'any_description',
+      is_active: true
+    })
+
+    expect(output).toStrictEqual({
+      id: repository.items[1].id,
+      name: 'any_category',
+      description: 'any_description',
+      is_active: true,
+      created_at: repository.items[1].created_at
+    })
+  })
+})
