9ae3e08251151ebe6ffdc1017ba3656993a02d5c
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-01-12 17:37:48 -0300

    feat: ensure DeleteCategoryUseCase deletes a category

--- src/application/usecases/category/index.ts
+++ src/application/usecases/category/index.ts
@@ -1,4 +1,5 @@
 export * from './create-category-usecase'
+export * from './delete-category-usecase'
 export * from './get-category-usecase'
 export * from './list-categories-use-case'
 export * from './update-category-usecase'
